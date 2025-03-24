57b35f8172320d795a608a34e4ff08bcc69dc557
VKasurinen <vaino.kasurinen@hotmail.fi>
2025-03-24 14:41:14 +0200

    responses from AI prompt

--- backend/src/routes/responses.py
+++ backend/src/routes/responses.py
@@ -1,22 +1,40 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException
+from src.db import get_database
+from bson import ObjectId
 
 router = APIRouter(prefix="/responses")
 
 # Get all responses
 @router.get("/")
 async def list_responses():
-    # TODO: get responses from db
-    return ["Response 1", "Response 2"]
+    db = get_database()
+    responses_collection = db.get_collection("responses")
+    responses = list(responses_collection.find())
+    
+    for response in responses:
+        response["_id"] = str(response["_id"])
+    
+    return responses
 
 # Get a response
 @router.get("/{response_id}")
-async def get_response(response_id: int):
-    # TODO: get response from db
-    return f"Response {response_id}"
+async def get_response(response_id: str):
+    db = get_database()
+    responses_collection = db.get_collection("responses")
+    response = responses_collection.find_one({"_id": ObjectId(response_id)})
+    
+    if response:
+        response["_id"] = str(response["_id"])
+        return response
+    raise HTTPException(status_code=404, detail="Response not found")
 
 # Delete a response
 @router.delete("/{response_id}")
-async def delete_response(response_id: int):
-    # TODO: delete response from db
-    return f"Response {response_id} deleted"
+async def delete_response(response_id: str):
+    db = get_database()
+    responses_collection = db.get_collection("responses")
+    result = responses_collection.delete_one({"_id": ObjectId(response_id)})
+    
+    if result.deleted_count == 1:
+        return {"message": f"Response {response_id} deleted"}
+    raise HTTPException(status_code=404, detail="Response not found")
